b74b785d7f2b9e9b4d01d48f9c82bdf6c901cabf
yunusbedir <yunusbedir@outlook.com.tr>
2022-10-11 23:19:35 +0300

    imported base module

---
+++ app/src/main/java/com/yunusbedir/playground/ui/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.yunusbedir.playground.ui
+
+import com.yunusbedir.base.ui.BaseActivity
+import com.yunusbedir.playground.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+
+    override fun setupUI() {
+
+    }
+
+    override fun setupCollectAndObserve() {
+
+    }
+
+    override fun setupListeners() {
+
+    }
+
+}
---
+++ app/src/main/java/com/yunusbedir/playground/ui/MainActivityViewModel.kt
@@ -0,0 +1,6 @@
+package com.yunusbedir.playground.ui
+
+import com.yunusbedir.base.ui.BaseViewModel
+
+class MainActivityViewModel : BaseViewModel() {
+}
