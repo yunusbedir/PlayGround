70e7619b9a306a6e52910ec7f23c5e52d9352dc9
yunusbedir <yunusbedir@outlook.com.tr>
2022-10-12 22:16:19 +0300

    imported splashScreen

--- app/src/main/java/com/yunusbedir/playground/ui/MainActivity.kt
+++ app/src/main/java/com/yunusbedir/playground/ui/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.yunusbedir.playground.ui
 
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.yunusbedir.base.ui.BaseActivity
 import com.yunusbedir.playground.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
+    private val viewModel: MainActivityViewModel by viewModels()
+
+    override fun onBeforeSetContent() {
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
+    }
 
     override fun setupUI() {
-
     }
 
     override fun setupCollectAndObserve() {
